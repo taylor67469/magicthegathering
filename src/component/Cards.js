a54ab35d489e72d1139a29daab14f0f833344e28
zach taylor <taylor67469@yahoo.com>
2021-12-14 19:14:57 -0600

    fixed random cards

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -1,25 +1,26 @@
 import "./cards.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import mtg  from 'mtgsdk';
+
 const Cards = () => {
     const [card, setCard] = useState([""]);
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
     }
     const newCards = () => {
-        // axios.get("https://api.magicthegathering.io/v1/cards",{
-        // })
-        // .then(data=>{console.log(data)})
-        axios.get(`https://api.magicthegathering.io/v1/cards`)
+let myNum = getRandomInt(1000)
+        axios.get(`https://api.magicthegathering.io/v1/cards?page=${myNum}`)
             .then(data => {
                 
-                let myNum = getRandomInt(data.data.cards.length)
+                
                 setCard(data.data.cards);
+                console.log(myNum);
             })
     }
     return (<div>
         <button className="myCards" onClick={newCards}>
-            New set
+            New set of cards
         </button>
         <br />
         <br/>
